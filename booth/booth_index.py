1d4cb552ec532b611b455040ed01eb1b31368f62
16st58 <105636718+16st58@users.noreply.github.com>
2022-12-18 11:18:24 +0900

    쿠폰 발행 조건 개선

--- booth/booth_index.py
+++ booth/booth_index.py
@@ -6,10 +6,9 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 import time
-import random
+import json
 
-boothName = '부스'
-boothOwner = '동아리'
+boothName = '동아리'
 
 boothList = ['동아리', '동아리1', '동아리2', '동아리3', '동아리4']
 userName = []
@@ -44,6 +43,35 @@ def addOneToDatabase(url, key):
     i = i + 1
     dir.update({key : i})
 
+def getPointCheck(barcode_info):
+    """
+    부스json파일을 읽고 쿠폰을 받을 수 있는 조건인지 검사, 이후 쿠폰 발행
+    @param barcode_info : 바코드 번호(개인 번호)
+    """
+    file_path = "booth\\booth.json"
+
+    with open(file_path, 'r', encoding='UTF8') as file:
+        data = json.load(file)
+    for i in data["쿠폰조건"]:
+        clear = True
+        for j in data["쿠폰조건"][i]:
+            alreadyBooth =  db.reference("user/" + barcode_info + "/이미방문한부스").get()
+            if alreadyBooth != None and data["쿠폰조건"][i][j] in alreadyBooth:
+                clear = False
+                break
+            if data["쿠폰조건"][i][j] not in db.reference("user/" + barcode_info + "/방문한부스/").get():
+                clear = False
+                break
+        if clear == True:
+            nowCoupon = db.reference("user/" + barcode_info + "/쿠폰").get()
+            updateDataToDatabase("user/" + barcode_info, "쿠폰", int(nowCoupon) + 100)
+            if alreadyBooth == None:
+                alreadyBooth = []
+            for j in data["쿠폰조건"][i]:
+                alreadyBooth.append(data["쿠폰조건"][i][j])
+            print(alreadyBooth)
+            updateDataToDatabase("user/" + barcode_info, "이미방문한부스", alreadyBooth)
+
 
 def read_barcodes(frame):
     """
@@ -65,49 +93,23 @@ def read_barcodes(frame):
         if(barcode_info.isdigit()):
             with open("barcode_result.txt", mode='w') as file:
                 file.write("Recognized Barcode:" + barcode_info)
+
                 if barcode_info not in userName:
                     """
                     바코드가 처음 인식 되었을 때
                     """
                     userName.append(barcode_info)
                     userTime.append(time.time())
-                    updateDataToDatabase("user/" + barcode_info + "/" + boothOwner + "/", "방문수", 1)
-                    
-                    #여러 부스 중에서 랜덤으로 3개의 부스를 뽑아 DB에 저장
-                    #여기에서 결정한 배열을 모두 방문했을 경우 쿠폰 증정
-                    try:
-                        if db.reference("user/" + barcode_info + "/쿠폰조건/").get() not in boothName:
-                            #randomBoothList = random.sample(boothList, 3)
-                            #updateDataToDatabase("user/" + barcode_info + "/", "쿠폰조건", randomBoothList)
-                            updateDataToDatabase("user/" + barcode_info + "/", "쿠폰조건", ["동아리"])
-                    except:
-                        #randomBoothList = random.sample(boothList, 3)
-                        #updateDataToDatabase("user/" + barcode_info + "/", "쿠폰조건", randomBoothList)
-                        updateDataToDatabase("user/" + barcode_info + "/", "쿠폰조건", ["동아리"])
+                    updateDataToDatabase("user/" + barcode_info + "/방문한부스/" + boothName + "/", "방문수", 1)
+                    getPointCheck(barcode_info)
+
 
                 elif time.time() - userTime[userName.index(barcode_info)] > 60:
                     #바코드가 인식되었을 때
                     userTime[userName.index(barcode_info)] = time.time()
-                    addOneToDatabase("user/" + barcode_info + "/" + boothOwner + "/", "방문수")
-
-                    visitedBooth = db.reference("user/" + barcode_info + "/쿠폰조건/").get()
-                    
-                    if boothName in visitedBooth:
-                        updateDataToDatabase("user/" + barcode_info + "/쿠폰조건/", visitedBooth.index(boothName), None)
-                    
-                    print(visitedBooth)
-                    
-                    try:
-                        if len(visitedBooth) == 1:
-                            nowCoupon = db.reference("user/" + barcode_info + "/쿠폰").get()
-                            updateDataToDatabase("user/" + barcode_info, "쿠폰", int(nowCoupon) + 100)
-                            db.reference("user/" + barcode_info + "/쿠폰조건/").delete()
-                    except:
-                        nowCoupon = db.reference("user/" + barcode_info + "/쿠폰").get()
-                        updateDataToDatabase("user/" + barcode_info, "쿠폰", int(nowCoupon) + 100)
-                        db.reference("user/" + barcode_info + "/쿠폰조건/").delete()
-
-                updateDataToDatabase("user/" + barcode_info + "/" + boothOwner + "/", "방문시간", time.time())
+                    addOneToDatabase("user/" + barcode_info + "/" + boothName + "/방문한부스/", "방문수")
+                    getPointCheck(barcode_info)
+                updateDataToDatabase("user/" + barcode_info + "/방문한부스/" + boothName + "/", "방문시간", time.time())
     # return the bounding box of the barcode
     return frame
 
